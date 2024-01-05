5b120188589f8cc875e1432616e425560dbe2cfe
kader01agi <md.abdul.kader.ru@gmail.com>
2024-01-05 19:30:53 +0600

    Django5.0 CRUD

--- portfolio/myportfolio/urls.py
+++ portfolio/myportfolio/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('about/insert/', views.about_insert, name='about_insert')
+    path('about/insert/', views.about_insert, name='about_insert'),
+    path('about/edit/<int:id>', views.about_edit, name='about_edit'),
+    path('about/edited/', views.edited, name='edited'),
+    path('about/delete/<int:id>', views.delete, name='delete'),
 ]
--- portfolio/myportfolio/views.py
+++ portfolio/myportfolio/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from . models import About
 
@@ -9,7 +9,9 @@ def home(request):
     return render (request, 'myportfolio/index.html')
 
 def about(request):
-    return render (request, 'admin/about.html')
+    allData = About.objects.all()
+    data = {'key': allData}
+    return render (request, 'admin/about.html', data)
 
 def about_insert(request):
     u_name = request.POST.get('u_name')
@@ -40,4 +42,50 @@ def about_insert(request):
 
     about_obj.save()
 
+    return redirect('about')
+
+
+def about_edit(request, id):
+    editable = About.objects.get(id = id)
+    editdata = {'d': editable}
+    return render (request, 'admin/edit.html', editdata)
+
+def edited(request):
+    id = request.POST.get('id')
+    u_name = request.POST.get('u_name')
+    dob = request.POST.get('dob')
+    phone = request.POST.get('phone')
+    email = request.POST.get('email')
+    no_exp = request.POST.get('exp')
+    no_happy_customers = request.POST.get('customers')
+    no_project_finished = request.POST.get('projects')
+    no_digital_awards = request.POST.get('awards')
+    description = request.POST.get('description')
+    date_time  = datetime.now( )
+    formatted_datetime = date_time.strftime("%d %b %Y - %I:%M %p")
+
+
+    about_obj = About.objects.get(id = id)
+
+    about_obj.u_name = u_name
+    about_obj.dob = dob
+    about_obj.phone = phone
+    about_obj.email = email
+    about_obj.no_exp = no_exp
+    about_obj.no_happy_customers = no_happy_customers
+    about_obj.no_project_finished = no_project_finished
+    about_obj.no_digital_awards = no_digital_awards
+    about_obj.description = description
+    # about_obj.date_time = formatted_datetime
+
+    about_obj.save()
+
+    return redirect('about')
+
+    # return render (request, 'admin/about.html')
+
+
+def delete(request, id):
+    deleteable = About.objects.get(id = id)
+    deleteable.delete()
     return redirect('about')
